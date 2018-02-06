33d82d3e88c692f63247d8f4ad17a7e64ee20bd5
edubwitowski <31215451+edubwitowski@users.noreply.github.com>
2018-02-06 11:14:40 -0600

    orientations

--- OrientationsEricW/ViewController.swift
+++ OrientationsEricW/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask(rawValue: (UIInterfaceOrientationMask.portrait.rawValue | UIInterfaceOrientationMask.landscapeRight.rawValue))
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,4 +25,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
